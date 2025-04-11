e3bc83ab6627e0355a64b9d506cb8c372976a3f6
Erick Christian <erickchristian48@gmail.com>
2025-04-11 17:39:34 +0700

    feat: add browser automation with playwright and tiny-invariant

---
+++ src/lib/browser.ts
@@ -0,0 +1,21 @@
+import { chromium } from "playwright"
+import invariant from "tiny-invariant"
+import { x } from "tinyexec"
+
+import { sleep } from "./sleep"
+
+export const spawnChromium = async () => {
+  x("chromium", ["--remote-debugging-port=9222"])
+  await sleep(10_000)
+}
+
+export const createPage = async () => {
+  const browser = await chromium.connectOverCDP("http://localhost:9222", {})
+  const context = browser.contexts().at(0)
+  invariant(context, "Default context not found")
+
+  const page = context.pages().at(0)
+  invariant(page, "Default page not found")
+
+  return page
+}
